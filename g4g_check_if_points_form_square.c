3fb2910ead68168adf46312be51d4e0551e4b10c
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2016-12-23 10:59:12 +0530

    Issue on g4g_is_square.c solved

--- g4g_check_if_points_form_square.c
+++ g4g_check_if_points_form_square.c
@@ -1,34 +1,37 @@
-#include <stdio.h>
-#include <math.h>
-struct point{
-    int x;
-    int y;
+#include<stdio.h>
+struct Point{
+    int x, y;
 };
-double findDistance(int a,int b,int c, int d){
-    double x;
-    x=sqrt((pow(fabs(c-a),2)+pow(fabs(d-b),2)));
-    return x;
+int distSq(struct Point p,struct Point q){
+    return ((p.x - q.x)*(p.x - q.x)+(p.y - q.y)*(p.y - q.y));
+}
+int isSquare(struct Point p1, struct Point p2, struct Point p3, struct Point p4){
+    int d2,d3,d4,d;
+    d2 = distSq(p1, p2);
+    d3 = distSq(p1, p3);
+    d4 = distSq(p1, p4);
+    if (d2 == d3 && 2*d2 == d4){
+        d = distSq(p2, p4);
+        return (d == distSq(p3, p4) && d == d2);
+    }
+    if (d3 == d4 && 2*d3 == d2){
+        d = distSq(p2, p3);
+        return (d == distSq(p2, p4) && d == d3);
+    }
+    if (d2 == d4 && 2*d2 == d3){
+        d = distSq(p2, p3);
+        return (d == distSq(p3, p4) && d == d2);
+    }
+    return 0;
 }
 int main(){
-int t;
-scanf("%d",&t);
-while(t--){
-    struct point p1,p2,p3,p4;
-    scanf("%d %d %d %d %d %d %d %d",&(p1.x),&(p1.y),&(p2.x),&(p2.y),&(p3.x),&(p3.y),&(p4.x),&(p4.x));
-    double d1,d2,d3;
-    d1=findDistance(p1.x,p1.y,p2.x,p2.y);
-    d2=findDistance(p1.x,p1.y,p3.x,p3.y);
-    d3=findDistance(p1.x,p1.y,p4.x,p4.y);
-    if(
-       ((d1==d2) && (d1*d1*2==d3*d3))
-       || ((d1==d3) && (d1*d1*2==d2*d2))
-       || ((d2==d3) && (d2*d2*2==d1*d1))
-       ){
-        printf("1\n");
-       }
-    else{
-        printf("0\n");
+    int t;
+    struct Point p1,p2,p3,p4;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d %d %d %d %d %d %d %d",&(p1.x),&(p1.y),&(p2.x),&(p2.y),&(p3.x),&(p3.y),&(p4.x),&(p4.y));
+        printf("%d\n",isSquare(p1,p2,p3,p4));
     }
-}
-return 0;
+
+    return 0;
 }
